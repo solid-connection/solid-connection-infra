b9e3fd9e89c0e496b8538e67a7dd9440d3f348a1
Hexeong <123macanic@naver.com>
2026-04-29 00:24:56 +0900

    fix: 코드 리뷰 반영 및 prod 환경에 대한 terraform 워크플로우 수정

--- bootstrap/iam.tf
+++ bootstrap/iam.tf
@@ -29,11 +29,20 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  tags = {
+    Project = "solid-connection"
+    Env     = "bootstrap"
+  }
 }
 
 resource "aws_iam_role" "github_actions" {
   name        = "GitHubActionsTerraformRole"
   description = "IAM Role for GitHub Actions terraform plan/apply via OIDC"
+  tags = {
+    Project = "solid-connection"
+    Env     = "bootstrap"
+  }
+
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -46,7 +55,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:solid-connection/solid-connection-infra:*"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:solid-connection/solid-connection-infra:ref:refs/heads/main",
+            "repo:solid-connection/solid-connection-infra:pull_request"
+          ]
         }
       }
     }]
